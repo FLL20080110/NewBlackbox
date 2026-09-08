785438bdee11970e142a920161527394419e5dc1
FLL20080110 <xfghvxdfy@163.com>
2026-09-08 19:10:12 +0800

    fix: make guest crash handler recursion-safe

--- Bcore/src/main/java/top/niunaijun/blackbox/core/CrashHandler.java
+++ Bcore/src/main/java/top/niunaijun/blackbox/core/CrashHandler.java
@@ -1,25 +1,58 @@
 package top.niunaijun.blackbox.core;
 
+import android.util.Log;
+
 import top.niunaijun.blackbox.BlackBoxCore;
 
-
+/**
+ * Guest-process crash bridge. Keep this handler deliberately small: virtual processes may be in a
+ * partially initialized or partially torn-down state when an exception reaches this point.
+ */
 public class CrashHandler implements Thread.UncaughtExceptionHandler {
-    private Thread.UncaughtExceptionHandler mDefaultHandler;
+    private static final String TAG = "BlackBoxCrashHandler";
+    private final Thread.UncaughtExceptionHandler mDefaultHandler;
 
     public static void create() {
-        new CrashHandler();
+        Thread.UncaughtExceptionHandler current = Thread.getDefaultUncaughtExceptionHandler();
+        if (!(current instanceof CrashHandler)) {
+            new CrashHandler(current);
+        }
     }
 
     public CrashHandler() {
-        mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        this(Thread.getDefaultUncaughtExceptionHandler());
+    }
+
+    private CrashHandler(Thread.UncaughtExceptionHandler defaultHandler) {
+        mDefaultHandler = defaultHandler;
         Thread.setDefaultUncaughtExceptionHandler(this);
     }
 
     @Override
     public void uncaughtException(Thread t, Throwable e) {
-        if (BlackBoxCore.get().getExceptionHandler() != null) {
-            BlackBoxCore.get().getExceptionHandler().uncaughtException(t, e);
+        try {
+            Thread.UncaughtExceptionHandler handler = BlackBoxCore.get().getExceptionHandler();
+            if (handler != null && handler != this && handler != mDefaultHandler) {
+                handler.uncaughtException(t, e);
+            }
+        } catch (Throwable callbackError) {
+            Log.e(TAG, "Exception callback failed", callbackError);
         }
-        mDefaultHandler.uncaughtException(t, e);
+
+        try {
+            if (mDefaultHandler != null && mDefaultHandler != this) {
+                mDefaultHandler.uncaughtException(t, e);
+                return;
+            }
+        } catch (Throwable defaultError) {
+            Log.e(TAG, "Default crash handler failed", defaultError);
+        }
+
+        // Last-resort termination avoids recursive uncaught-exception loops and frozen guest slots.
+        try {
+            android.os.Process.killProcess(android.os.Process.myPid());
+        } catch (Throwable ignored) {
+        }
+        System.exit(10);
     }
 }
